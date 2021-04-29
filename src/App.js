1f1e06f4ec5af3395f1f63cdcbb34f0f2a9dad0e
stefzouille <contact.lokcy@gmail.com>
2021-04-29 17:54:14 +0200

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react'
+import "bootstrap/dist/css/bootstrap.css"
+import './App.css';
+import StarWarsApp from "./components/StarWarsApp"
+import {useState } from 'react'
+
+
+
+const  App = () => {
+    const [url, setUrl] = useState('http://swapi.dev/api/planets/')
+    const[nextUrl, setNextUrl] = useState('')
+
+  const handleButtonMore = () => {
+    setUrl(nextUrl)
+  }
+const handleButtonClick = () => {
+  console.log(url)
+  console.log(nextUrl)
+}
+
+    return (
+      <div className="App">
+        <h1 className='display-1 text-center'>Les planetes starwars</h1>
+        <div className="container">
+          <StarWarsApp setNextUrl={setNextUrl} url={url}/>
+          <button disabled={nextUrl === null} onClick={handleButtonMore} className="btn btn-info my-5 me-2">{nextUrl !== null? "Voir plus" : "Fin de la liste"}</button>
+          <button onClick={handleButtonClick} className="btn btn-info my-5">Voir les données (console)</button>
+        </div>
+      </div>
+    );
+  
 }
 
 export default App;
---
+++ src/components/StarWarsApp.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+
+const StarWarsApp = ({ url, setNextUrl }) => {
+  const [planetsList, setPlanetsList] = useState([])
+  const [isloading, setIsLoading] = useState(false)
+
+
+useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true)
+      let data = await axios(url)
+      setIsLoading(false)
+      console.log(data)
+      console.log(data.data.results)
+      console.log(data.next)
+  //agrandir la liste
+  for (let elem of data.data.results) {
+    planetsList.push(elem)
+  }
+
+      setPlanetsList(() => planetsList)
+      setNextUrl(() => data.data.next)
+    }
+    fetchData()
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+}, [url])
+
+
+  return ( <div>
+    
+        <div className="d-flex flex-wrap">
+          
+          {planetsList.map((elem) => {
+            return <div key={elem.name} className="card mb-3 me-3" style={{width:'200px', backgroundColor:'yellow'}}>
+              <div className="card-body">
+                <h2 className="card-title">{elem.name}</h2>
+                <h3 className="card-subtitle">Diamètre : {elem.diameter}</h3>
+              </div>
+            </div>
+          })}
+          </div>
+          {isloading ? <h2>Chargement...</h2> : '' }
+          </div>
+        
+  )
+}
+export default StarWarsApp
